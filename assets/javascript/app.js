c5d812714bfbc618b08078c45047e9437de0fb6d
cnpham <dracuong@yahoo.com>
2018-12-12 20:23:21 -0500

    hi

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+// initialize Firebase
+var config = {
+    apiKey: "AIzaSyCUa3OmzBQAV9MHxQg6Pgl2s5533V5qjEI",
+    authDomain: "coder-bay-fee9d.firebaseapp.com",
+    databaseURL: "https://coder-bay-fee9d.firebaseio.com",
+    storageBucket: "trainscheduler-d8b8f.appspot.com"
+  };
+
+firebase.initializeApp(config);
+
+// variable reference to database
+var database = firebase.database();
+
+
+  
